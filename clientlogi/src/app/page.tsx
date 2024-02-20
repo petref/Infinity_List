9f151dadb3176383b5fde662299611eadc78e875
petref <petroupb@gmail.com>
2024-02-20 21:07:47 +0200

    Add home landing page

--- clientlogi/src/app/page.tsx
+++ clientlogi/src/app/page.tsx
@@ -1,9 +1,25 @@
-import Image from "next/image";
+import { useEffect, useState } from "react";
 import styles from "./page.module.css";
 
 export default function Home() {
+
+  const [items, setItems] = useState<{
+    index: number;
+    name: string;
+    checked: boolean;
+} | (() => { index: number; name: string; checked: boolean; })>([]);
+
+  useEffect(() => {
+    // repopulate the list when count changes
+    setItems(
+      new Array(500000)
+        .fill(null)
+        .map((_, i) => ({ index: i, name: `Item ${i}`, checked: false }))
+    );
+  }, []);
   return (
     <main className={styles.main}>
+      
     </main>
   );
 }
