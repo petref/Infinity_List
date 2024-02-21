53ea4c32053cc468034e1cd48d9a261078d3d81c
petref <petroupb@gmail.com>
2024-02-21 23:00:04 +0200

    Add server

--- server/index.js
+++ server/index.js
@@ -2,18 +2,19 @@ var express = require('express');
 var cors = require('cors');
 var app = express();
 
-const {tableData} = require("./utils");
+const {tableData , addOneRow} = require("./utils");
 
 require('dotenv').config()
 app.use(express.json());
 app.use(cors());
 const PORT = process.env.PORT || 5000;
 
-app.get('/getTableData', function (req, res, next) {
-    const status = {
-        "Status": "Running",
-     };
+app.get('/getTableData',  (req, res, next) => {
      res.send(tableData());
+});
+
+app.post('/addOneRow', ({body}, res, next) => {
+   res.send(addOneRow(body?.tableRow || 0 + 1));
 });
 
 app.listen(PORT, () => {
--- server/utils.js
+++ server/utils.js
@@ -1,8 +1,11 @@
 const tableData = () => new Array(100000)
             .fill(null)
-            .map((_, i) => ({ index: i, name: `Item ${i}`, checked: false }))
+            .map((_, i) => ({ index: i, name: `Item ${i+1}`, description: `Item ${i+1} description`, price: `Item ${i+1} price` }))
+
+const addOneRow = (rows) => rows += 1;
 
 
 module.exports  = {
-    tableData
+    tableData,
+    addOneRow
 }
