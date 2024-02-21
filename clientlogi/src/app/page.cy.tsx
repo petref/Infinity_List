c4b93646a5c7b16c9cecc02999e02885c03aefa1
petref <petroupb@gmail.com>
2024-02-21 23:06:08 +0200

    add page body

---
+++ clientlogi/src/app/page.cy.tsx
@@ -0,0 +1,10 @@
+import Home from './page'
+
+import "./page.module.css"
+
+describe('<Home />', () => {
+  it('renders', () => {
+    // see: https://on.cypress.io/mounting-react
+    cy.mount(<Home />)
+  })
+})
--- clientlogi/src/app/page.tsx
+++ clientlogi/src/app/page.tsx
@@ -1,9 +1,41 @@
 "use client"
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./page.module.css";
 
-import VirtuaList from "../components/VirtuaList";
-import { ItemsState } from "@/types/types";
+import { getTableData } from "../services/tableListActions";
+import VirtuaList from "../components/VirtuaList/VirtuaList";
+import { ItemsState, DataRow } from "../types/types";
+
+
+export const DataRows: React.FC<DataRow> = ({ style, item} : DataRow) =>  (
+    <div 
+        key={item?.name} 
+        className={styles.item} 
+        style={style}
+    >
+        <div className={styles.itemWrapper}>
+            <div className={styles.cell}>
+                {item?.name}
+            </div>
+            <div className={styles.cell}>
+                {item?.description}
+            </div>
+            <div className={styles.cell}>
+                {item?.price}
+            </div>
+        </div>
+    </div>
+);
+
+const addRow = (items: ItemsState[]): ItemsState[] => [
+    ...items,
+    {
+        index: items.length + 1,
+        name: `Item ${items.length + 1}`,
+        description: `Item ${items.length + 1}`,
+        price:  `Item ${items.length + 1}`,
+    }
+  ];
 
 
 export default function Home() {
@@ -11,48 +43,36 @@ export default function Home() {
   const [items, setItems] = useState<ItemsState[]>([]);
 
   useEffect(() => {
-    // repopulate the list when count changes
-    setItems(
-      new Array(500000)
-        .fill(null)
-        .map((_, i) => ({ index: i, name: `Item ${i}`, checked: false }))
-    );
+    getTableData()
+    .then(res => {setItems(res)});
   }, []);
+
+
+
   return (
     <main className={styles.main}>
         <div className={styles.titleMenuWrapper}>
             <div className={styles.titlePage}>
-                <h3>Virtualized list</h3>
+                <h3>VIrtualized list</h3>
             </div>
-            <button className={styles.button}>Add new item</button>
+            <button
+                type="button"
+                onClick={() => setItems(addRow(items))} 
+                className={styles.button}>
+                    Add new item
+            </button>
         </div>
         <div className={styles.listWrapper}>
             <VirtuaList 
-                totalItems={500000}
+                totalItems={items.length}
                 itemHeight={80}
                 windowHeight={800}
-                renderItem={({ index, style } : { index: number, style: object }) => {
-                    const item = items[index];
-                    return (
-                    <div 
-                        key={item?.name} 
-                        className={styles.item} 
+                renderItem={({ index, style } : { index: number, style: object }) => ( 
+                    <DataRows 
                         style={style}
-                    >
-                        <div className={styles.itemWrapper}>
-                            <div className={styles.cell}>
-                                {item?.name}
-                            </div>
-                            <div className={styles.cell}>
-                                {item?.name}
-                            </div>
-                            <div className={styles.cell}>
-                                {item?.name}
-                            </div>
-                        </div>
-                    </div>
-                    );
-                }}
+                        item={items[index]}
+                    />
+                )}
             />
         </div>
     </main>
